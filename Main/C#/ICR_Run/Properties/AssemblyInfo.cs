a80e7429f1566ce001a8f59f5174a335a8c403be
lester <lester@email.arizona.edu>
2017-09-08 15:15:23 -0700

    Udating to use new NetCom

--- Main/C#/ICR_Run/Properties/AssemblyInfo.cs
+++ Main/C#/ICR_Run/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ICR_HubProgram")]
+[assembly: AssemblyTitle("ICR_Run")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ICR_HubProgram")]
+[assembly: AssemblyProduct("ICR_Run")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
